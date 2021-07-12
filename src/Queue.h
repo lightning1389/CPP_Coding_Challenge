49b46608f06ae2c0370e674dbddb7f63a4daab7a
Ilhan <ilhan.dogan@gmx.de>
2021-07-12 18:39:55 +0200

    member variable renamed for easier overview

--- src/Queue.h
+++ src/Queue.h
@@ -20,14 +20,10 @@ template<typename T>
 class Queue
 {
     std::condition_variable condition;
-    int capacity = 0;  // Max capacity
-    T *arr;
-    int i = 0; 
-    int head = 0, tail = 0;
-    int size = 0;   // actual size
-    int count = 0;  
-    //Queue* queue_;
-    //Queue() {};   
+    int m_capacity = 0;  // Max m_capacity
+    T *m_arr;
+    int m_head = 0, tail = 0;
+    int m_size = 0;   // actual m_size
     std::mutex mtx;  // protects the Queue
  
 public:
@@ -56,11 +52,11 @@ public:
  */
 template <typename T>
 Queue <T>::Queue(int sizeofqueue)
-    :capacity{sizeofqueue}
-    ,head{0}
-    ,size{0}
+    :m_capacity{sizeofqueue}
+    ,m_head{0}
+    ,m_size{0}
     ,tail{0}
-    ,arr{new T[sizeofqueue]}
+    ,m_arr{new T[sizeofqueue]}
     ,mtx{}
 {
 }
@@ -72,11 +68,11 @@ Queue <T>::Queue(int sizeofqueue)
  */
 template <typename T>
 Queue <T>::Queue(Queue const& other)
-    :capacity{other.capacity}
-    ,head{other.head}
-    ,size{other.size}
+    :m_capacity{other.m_capacity}
+    ,m_head{other.m_head}
+    ,m_size{other.m_size}
     ,tail{other.tail}
-    ,arr{other.arr = arr}	
+    ,m_arr{other.m_arr}	
     ,mtx{other.mtx}
 {
 }
@@ -91,17 +87,17 @@ void Queue<T>::Push(T element)
     std::unique_lock<std::mutex> mlock(mtx);
  
     // check if there is still enough space 
-    while (size == capacity)
+    while (m_size == m_capacity)
     {
         // FULL 
         condition.wait(mlock);   // (4)     
     }
  
 
-    tail = tail % capacity;
-    arr[tail] = element;
+    tail = tail % m_capacity;
+    m_arr[tail] = element;
     tail += 1; // nothing on tail now (as tail+1)
-    size += 1;
+    m_size += 1;
     sleepcp(20); 
     mlock.unlock();
     condition.notify_one();
@@ -118,18 +114,18 @@ T Queue<T>::Pop()
     std::unique_lock<std::mutex> mlock(mtx);
  
     // check if there are any elements 
-    while (size == Zero)
+    while (m_size == Zero)
     {
         // empty 
         condition.wait(mlock);
     }
     T temp;
-    temp = arr[head]; 
-    arr[head] = 0 ; 
+    temp = m_arr[m_head]; 
+    m_arr[m_head] = 0 ; 
  
-    head = head % capacity;
-    head += 1;
-    size -= 1;  // one slot free
+    m_head = m_head % m_capacity;
+    m_head += 1;
+    m_size -= 1;  // one slot free
     sleepcp(20); 
     mlock.unlock();
     condition.notify_one();
@@ -144,19 +140,19 @@ T Queue<T>::Pop()
 template<typename T>
 Queue <T>::~Queue()
 {
-	delete[] arr; // use array delete to deallocate array
+	delete[] m_arr; // use m_array delete to deallocate m_array
 }
 
  
 
 /**
  * Count
- * returns the max number of elements in the Queue == Capacity
+ * returns the max number of elements in the Queue == m_capacity
  */
 template<typename T>
 const int  Queue<T>::Count()    
 {
-    return (capacity);
+    return (m_capacity);
 }
  
 
@@ -167,27 +163,27 @@ const int  Queue<T>::Count()
 template<typename T>
 const int  Queue<T>::Size()
 {
-    return (size);
+    return (m_size);
 }
  
 
 /**
  * Prints Queue
  * DEBUG PURPOSE ONLY
- * A simple queue function that prints out size, capacity and current Queue
+ * A simple queue function that prints out size, m_capacity and current Queue
  * Be aware, that even the "deleted" queue spots will be printed
  * 
  */
 template<typename T>
 void Queue<T>::printfQueue()
 {
-    for (int i = 0; i < capacity; i++)
+    for (int i = 0; i < m_capacity; i++)
     {
-        std::cout << arr[i] << " ";
+        std::cout << m_arr[i] << " ";
     }
     std::cout << std::endl;
-    std::cout << "H & T: " << head << " " << tail << "; ";
-    std::cout << "size of queue " << size << std::endl;
+    std::cout << "H & T: " << m_head << " " << tail << "; ";
+    std::cout << "size of queue " << m_size << std::endl;
 }
  
 
