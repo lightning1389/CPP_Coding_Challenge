1b88ea6263a67f50ec131e5b1a88209dbcca3bca
Dogan Ilhan (CC-DA/ESI4) <ilhan.dogan@de.bosch.com>
2021-07-14 09:23:50 +0200

    Added consumer to main function

--- src/Source.cpp
+++ src/Source.cpp
@@ -11,7 +11,9 @@ int main()
     typedef Queue<float> QueueFloats;
     QueueInts queue(8);                        // Queue created with 7 elements
     std::thread produce1(produce<int>, std::ref(queue));    
+    std::thread consume1(consume<int>, std::ref(queue));
     produce1.join();
+    consume1.join(); 
     std::cout << "Result of Queue "; 
     queue.printfQueue(); 
     std::cout << std::endl << std::endl; 
